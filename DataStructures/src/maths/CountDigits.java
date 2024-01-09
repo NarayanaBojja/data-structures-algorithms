9edc706c2ed8cc4c82268194d9ce40e72f8a0e3d
Narayana <narayana.bojja143@gmail.com>
2024-01-09 23:38:56 +0530

    count digits in number

---
+++ DataStructures/src/maths/CountDigits.java
@@ -0,0 +1,15 @@
+package maths;
+
+public class CountDigits {
+
+	public static void main(String[] args) {
+		int number = 45789;
+		int count = 0;
+		while (number > 0) {
+			number /= 10;
+			count++;
+		}
+		System.out.println("Digits count: "+count);
+	}
+
+}
